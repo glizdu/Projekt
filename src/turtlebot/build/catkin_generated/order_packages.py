b533aaf5a456543619eacfab77559852e7343b25
Adrian Glinkowski <adrian.glinkowski@gmail.com>
2018-06-12 19:11:32 +0200

    Wrzucenie niezbędnych plików

--- src/turtlebot/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/glizdu/Pulpit/turtlebot/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/glizdu/ROS-labbot/labbot_ws/devel;/opt/ros/kinetic".split(';') if "/home/glizdu/ROS-labbot/labbot_ws/devel;/opt/ros/kinetic" != "" else []
